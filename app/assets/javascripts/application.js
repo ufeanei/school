ae8f7ab45b6c1c880d2a283a72501522c11eebcc
ufeanei <akaranjoh@yahoo.com>
2017-08-18 20:48:56 +0200

    added another page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require_tree .
+
 //= require turbolinks
 //= require_tree .
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/home" => "pages#hello"
+  get "/coursedetails" => "pages#coursedetails"
   root 'pages#hello'
 end
