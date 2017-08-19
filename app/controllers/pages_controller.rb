9f16aca56cee6c658990dc6e7f8d7bd6abdc8dba
ufeanei <akaranjoh@yahoo.com>
2017-08-19 04:03:17 +0200

    added many views and partials

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,25 @@ class PagesController < ApplicationController
 
   def hello
     
-    end
+  end
+
+  def webdesign
+  end
+
+  def android
+  end
+
+  def laravel
+  end
+
+  def railscourse
+  end
+
+  def gims
+  end
+
+  def diploma
+  end
     
  
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/home" => "pages#hello"
-  get "/coursedetails" => "pages#coursedetails"
+  get "/webdesign" => "pages#webdesign"
+
+  get "/android"  => "pages#android"
+  get "/laravel"  => "pages#laravel"
+  get "/railscourse"  => "pages#railscourse"
+  get "/diploma"  => "pages#diploma"
+
   root 'pages#hello'
 end
