6cb66e0a4dcc23919bbd419747ceb655b831f0df
ufeanei <akaranjoh@yahoo.com>
2017-09-08 22:39:37 +0200

    change db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,16 @@
 
 courses= Course.create([
 
-  {name: 'Professional Diploma in Android and Web Systems' }, 
-  {name: 'Global IT Infrastructure Management Specialist' }, 
-  {name: 'Android App Development'},
-  {name: 'Professional Mobile First Web Design'}, 
-  {name: 'Web Development with PHP and Laravel'}, 
-  {name: 'Web Development with Ruby on Rails'}, 
+  #{name: 'Professional Diploma in Android and Web Systems' }, 
+  #{name: 'Global IT Infrastructure Management Specialist' }, 
+  #{name: 'Android App Development'},
+  #{name: 'Professional Mobile First Web Design'}, 
+  #{name: 'Web Development with PHP and Laravel'}, 
+  #{name: 'Web Development with Ruby on Rails'}, 
+  {name: 'Hardware and Network Technician '},
+  {name: 'Network Adminstration (CCNA)'},
+  {name: 'Microsoft Server Administration '},
+  {name: 'Linux System Administration(Red Hat/Ubuntu)'},
   
   ])
 
